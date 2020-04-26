1559c95c8c2c4545c0db5c7ab889f48ceee3acec
joerdis101 <joerdis@bites-it.de>
2020-04-26 17:43:55 +0200

    make a generic user collection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { User } from "./models/User";
+import {User, UserProps} from "./models/User";
 
-const user = User.buildUser({id: 1, name: 'newer name', age: 12});
+const collection = User.buildUserCollection();
 
-user.on('change', () => {
-    console.log('user')
+collection.on('change', () => {
+    console.log(collection)
 });
 
-user.fetch();
+collection.fetch();
